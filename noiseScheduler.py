617df4f98c85ab186d2f86328ded8da6a083989f
Ishushan02 <ishananand2002@gmail.com>
2025-03-25 14:36:00 -0700

    DDPM Diffusion Model: Scheduler Modification

--- noiseScheduler.py
+++ noiseScheduler.py
@@ -1,69 +1,89 @@
 import torch
 
+
 class LinearNoiseScheduler:
-    '''
+    r"""
     In this function according to given formula of alpha, beta, 
     the cummulative product of alpha, we store those values and 
     also add noise to the given image with the above factors.
     We are storing it because we can directly use these values while in sampling procedure
     See the formula dewscription for detailed summary.
-    '''
-    def __init__(self, timeSteps, betaStart, betaEnd):
-        self.timeSteps = timeSteps
-        self.betaStart = betaStart
-        self.betaEnd = betaEnd
 
-        self.betas = torch.linspace(betaStart, betaEnd, timeSteps)
+    Class for the linear noise scheduler that is used in DDPM.
+    """
+    def __init__(self, num_timesteps, beta_start, beta_end):
+        self.num_timesteps = num_timesteps
+        self.beta_start = beta_start
+        self.beta_end = beta_end
+        
+        self.betas = torch.linspace(beta_start, beta_end, num_timesteps)
         self.alphas = 1. - self.betas
-        self.alpha_hat = torch.cumprod(self.alphas, dim=0)
-        self.sqrt_alpha_hat = torch.sqrt(self.alpha_hat)
-        self.sqrt_one_minus_alpha_hat = torch.sqrt(1 - self.alpha_hat)
-
-    def add_noise(self, image, noise, t):
-        '''
+        self.alpha_cum_prod = torch.cumprod(self.alphas, dim=0)
+        self.sqrt_alpha_cum_prod = torch.sqrt(self.alpha_cum_prod)
+        self.sqrt_one_minus_alpha_cum_prod = torch.sqrt(1 - self.alpha_cum_prod)
+        
+    def add_noise(self, original, noise, t):
+        r"""
         This is the forward function of adding noise to the image
         at timestep t, with certain noise.
         The image dimension would be (batch_size, channel, height, width)
 
         See the formula that we are applying for the forward process of adding Noise at time t.
-        '''
-        batch_size, channel, height, width = image.shape
 
-        # this becomes batch, 1, 1, 1
-        sqrt_alphar_hat = self.sqrt_alpha_hat[t].reshape(batch_size)
-        sqrt_one_minus_alpha_hat = self.sqrt_one_minus_alpha_hat[t].reshape(batch_size)
-
-        for _ in range(len(image.shape) -1):
-            sqrt_alphar_hat = sqrt_alphar_hat.unsqueeze(-1)
-            sqrt_one_minus_alpha_hat = sqrt_one_minus_alpha_hat.unsqueeze(-1)
-
-        return sqrt_alphar_hat * image + sqrt_one_minus_alpha_hat * noise
-    
-    
-    def sampling_step(self, image_t, noise_pred, t):
-        '''
+        Forward method for diffusion
+        :param original: Image on which noise is to be applied
+        :param noise: Random Noise Tensor (from normal dist)
+        :param t: timestep of the forward process of shape -> (B,)
+        :return:
+        """
+        original_shape = original.shape
+        batch_size = original_shape[0]
+        
+        sqrt_alpha_cum_prod = self.sqrt_alpha_cum_prod.to(original.device)[t].reshape(batch_size)
+        sqrt_one_minus_alpha_cum_prod = self.sqrt_one_minus_alpha_cum_prod.to(original.device)[t].reshape(batch_size)
+        
+        # Reshape till (B,) becomes (B,1,1,1) if image is (B,C,H,W)
+        for _ in range(len(original_shape) - 1):
+            sqrt_alpha_cum_prod = sqrt_alpha_cum_prod.unsqueeze(-1)
+        for _ in range(len(original_shape) - 1):
+            sqrt_one_minus_alpha_cum_prod = sqrt_one_minus_alpha_cum_prod.unsqueeze(-1)
+        
+        # Apply and Return Forward process equation
+        return (sqrt_alpha_cum_prod.to(original.device) * original
+                + sqrt_one_minus_alpha_cum_prod.to(original.device) * noise)
+        
+    def sample_prev_timestep(self, xt, noise_pred, t):
+        r"""
         We get image at time t, and also the predicted noise at time t
         Here in this step basically we get predicted noise from the Neural Network
         we have to remove it, we remove it.
         See the formula that we are applying for the sampling process of at time t.
-        '''
-        image = (image_t - (self.sqrt_one_minus_alpha_hat[t] * noise_pred))/ self.sqrt_alpha_hat
-        image = torch.clamp(image, min=-1., max=1.)
-
-        mean = (1/torch.sqrt(self.alphas[t])) * (image_t - ((self.betas[t] * noise_pred)/self.sqrt_one_minus_alpha_hat[t]))
-
-        if t==0:
-            # when t is 0, no variance just return mean and the image
-            return mean, image
         
-        # else valvulate variance using the formula and send it and send the gaussiaan of it
-        variance = (self.betas[t]) * (1 - self.alpha_hat[t-1])/(1 - self.alpha_hat[t])
-        sigma = variance ** 0.5
-        z = torch.randn(image_t.shape).to(image_t.device)
 
-        return  mean + sigma * z, image
-
-
-
-
-
+            Use the noise prediction by model to get
+            xt-1 using xt and the noise predicted
+        :param xt: current timestep sample
+        :param noise_pred: model noise prediction
+        :param t: current timestep we are at
+        :return:
+        """
+        x0 = ((xt - (self.sqrt_one_minus_alpha_cum_prod.to(xt.device)[t] * noise_pred)) /
+              torch.sqrt(self.alpha_cum_prod.to(xt.device)[t]))
+        x0 = torch.clamp(x0, -1., 1.)
+        
+        mean = xt - ((self.betas.to(xt.device)[t]) * noise_pred) / (self.sqrt_one_minus_alpha_cum_prod.to(xt.device)[t])
+        mean = mean / torch.sqrt(self.alphas.to(xt.device)[t])
+        
+        if t == 0:
+            return mean, x0
+        else:
+            variance = (1 - self.alpha_cum_prod.to(xt.device)[t - 1]) / (1.0 - self.alpha_cum_prod.to(xt.device)[t])
+            variance = variance * self.betas.to(xt.device)[t]
+            sigma = variance ** 0.5
+            z = torch.randn(xt.shape).to(xt.device)
+            
+            # OR
+            # variance = self.betas[t]
+            # sigma = variance ** 0.5
+            # z = torch.randn(xt.shape).to(xt.device)
+            return mean + sigma * z, x0
